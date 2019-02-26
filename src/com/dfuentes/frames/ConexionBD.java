d27a3f4247792c183c835c3bc9cabe88dd051878
wssolis <Santiago@DESKTOP-TTM0FH3>
2019-02-26 09:46:27 -0500

    Version Ssolis

--- src/com/dfuentes/frames/ConexionBD.java
+++ src/com/dfuentes/frames/ConexionBD.java
@@ -4,15 +4,17 @@ package com.dfuentes.frames;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 
 public class ConexionBD {
     Connection cn;
     Statement st;
     public Connection conexion(){
+        JOptionPane.showMessageDialog(null,"Ingresando al sistema");
         try {
              Class.forName("com.mysql.jdbc.Driver");
-             cn = DriverManager.getConnection("jdbc:mysql://localhost/programasistema","root","");
+             cn = DriverManager.getConnection("jdbc:mysql://localhost/sistema","root","rootSistem@s");
              System.out.println("Se hizo la conexión exitosa");
             
         } catch (Exception e) {
